571872eb988962f285f172ca6901ff6803005dbb
Youssef <youssef.bouzidi@etu.uca.fr>
2025-11-03 19:34:11 +0100

    Update CORS origins in main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def create_app() -> FastAPI:
 
     # === CONFIG CORS (insérée correctement DANS create_app) ===
     origins = [
-        "https://mosquee-arrahmane-web.lovable.app",  # <-- PAS de slash final
+        "https://mosquee-arrahmane-web.lovable.app",   # ton site Lovable
+        "https://lovable.dev/",   # <-- PAS de slash final
         # "http://localhost:5173",                   # <-- décommente si tu veux tester en local
     ]
 
